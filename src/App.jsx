27e62e641e980bad971b77fac67e745cdba74d32
Anjali Sharma <sharmaanjali1241@gmail.com>
2024-09-27 02:25:35 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useEffect, useMemo, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useMemo } from "react";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   const data = useMemo(() => {
     return "helloworld";
   }, []);
